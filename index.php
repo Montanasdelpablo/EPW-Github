363f6a68495e66c99177edc40624f248339715c5
Pablo Cleij <pablocley@hotmail.com>
2016-06-15 14:38:37 +0200

    Added main structure frontpage

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php 
+
+require 'header.php';
+
+?>
+
+
+<div id="maincontent1">
+	
+
+</div>
+
+<div id="maincontent2">
+	<div id="leftcontent2"> 
+	<h1> Portfolio <h1>
+
+	</div>
+	<div id="rightcontent2"> 
+
+
+	</div>
+
+</div>
+
+<div id="maincontent3">
+	<div id="leftcontent3"> 
+
+
+	</div>
+	<div id="rightcontent3"> 
+	<h1> About me <h1>
+
+	</div>
+
+</div>
+
+<div id="maincontent4">
+	<div id="leftcontent4"> 
+	<h1> My blog <h1>
+
+	</div>
+	<div id="rightcontent4"> 
+
+
+	</div>
+
+</div>
+
+
+
+<?php 
+
+require 'footer.php';
+
+?>
