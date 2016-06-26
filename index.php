78a864716ac6623131885d0ba65e15e626daf8de
Pablo Cleij <pablocley@hotmail.com>
2016-06-26 16:11:15 +0200

    Moved elements to frontpage

--- index.php
+++ index.php
@@ -5,58 +5,6 @@ require 'header.php';
 ?>
 
 
-<div id="maincontent1">
-	<div id="introduction">
-	<h1> Welcome to my Website! </h1>
-	<h4> Feel free to look around. Scroll down if you want to take a fast look. <br> Any questions? Contact me.</h4>
-
-	<div id="calltoaction">
-
-		<button class="introbutton"> <a href="#contact"> </a> MY PORTFOLIO </button>
-		<button class="introbutton"> <a href="#myportfolio"> </a> CONTACT ME </button>
-
-	</div>
-	</div>
-
-	
-
-</div>
-
-<div id="maincontent2">
-	<div id="leftcontent2"> 
-	<h1> Portfolio <h1>
-
-	</div>
-	<div id="rightcontent2"> 
-
-
-	</div>
-
-</div>
-
-<div id="maincontent3">
-	<div id="leftcontent3"> 
-
-
-	</div>
-	<div id="rightcontent3"> 
-	<h1> About me <h1>
-
-	</div>
-
-</div>
-
-<div id="maincontent4">
-	<div id="leftcontent4"> 
-	<h1> My blog <h1>
-
-	</div>
-	<div id="rightcontent4"> 
-
-
-	</div>
-
-</div>
 
 
 
