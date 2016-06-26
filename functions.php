e16fbf553a8b3c7d278b13b25393930ba34e4c62
Pablo Cleij <pablocley@hotmail.com>
2016-06-26 18:49:23 +0200

    Added theme support, image sizes and created CPT

--- functions.php
+++ functions.php
@@ -3,6 +3,14 @@
 // show admin bar
  show_admin_bar( false ); 
 
+// add theme support
+add_theme_support( 'post-thumbnails' );
+
+// image sizes
+
+add_image_size ('portfolio-item-frontpage', 230, 180);
+
+
 // enqueue scripts
  function enqueuescripts () {
  	wp_enqueue_script("jquery");
@@ -16,5 +24,23 @@
 
 // include jquery 
 
+
+// custom post types
+
+add_action( 'init', 'create_posttype' );
+function create_posttype() {
+  register_post_type( 'portfolio',
+    array(
+      'labels' => array(
+        'name' => __( 'Portfolio' ),
+        'singular_name' => __( 'Portfolio items' )
+      ),
+      'public' => true,
+      'has_archive' => true,
+      'rewrite' => array('slug' => 'portfolio'),
+      'supports' => array('thumbnail', 'title', 'editor', 'author', 'comments'),
+    )
+  );
+}
 
 ?>
