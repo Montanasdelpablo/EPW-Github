dd58d8d8844c27074278941fb12e3815ff57295b
Pablo Cleij <pablocley@hotmail.com>
2016-06-26 16:10:20 +0200

    Added frontpage

---
+++ front-page.php
@@ -0,0 +1,101 @@
+<?php 
+
+require 'header.php';
+
+
+?>
+
+<?php get_template_part("sidebar-nav"); ?>
+
+<div id="page-content-wrapper">
+
+ <button type="button" class="hamburger is-closed" data-toggle="offcanvas">
+                <span class="hamb-top"></span>
+    			<span class="hamb-middle"></span>
+				<span class="hamb-bottom"></span>
+            </button>
+	
+
+<div class="container-fluid" id="maincontent1">
+	<div class="row row-centered">
+		<div class="col-md-12" id="introduction">
+		<h1> Welcome to my Website! </h1>
+		<h4> Feel free to look around. Scroll down if you want to take a fast look. <br> Any questions? Contact me.</h4>
+	</div>
+
+	<div class="row row-centered">
+	 	<div class="col-md-12" id="calltoaction">
+
+		<button class="introbutton"> <a href="#contact"> </a> MY PORTFOLIO </button>
+		<button class="introbutton"> <a href="#myportfolio"> </a> CONTACT ME </button>
+		</div>
+	</div>
+</div>
+
+	
+
+</div>
+
+<div class="container-fluid" id="maincontent2">
+	<div class="row">
+		<div class="col-md-6" id="leftcontent2"> 
+		<h1> Portfolio <h1>
+		<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ac fringilla enim, vitae ultrices justo. Curabitur in dolor interdum, feugiat augue vel, congue purus. Quisque vitae diam turpis. Sed vestibulum porta augue, id interdum tellus interdum consectetur. Etiam sagittis nisi vel mi rutrum elementum. In non placerat nibh, eget dignissim urna. In malesuada risus arcu, sed ornare massa tempus ac. Sed sodales sapien id eros congue, at dictum risus laoreet. Duis sed quam eu nibh lobortis dapibus sed eget quam. Aliquam finibus, magna consectetur convallis gravida, nibh tortor eleifend nisi, in congue lorem lacus vitae sapien. Nunc ac efficitur dui, porta bibendum odio.
+
+</p>
+		</div>
+		
+		<div class="col-md-6" id="rightcontent2"> 
+
+
+		</div>
+
+	</div>
+</div>
+
+<div class="container-fluid" id="maincontent3">
+	<div class="row">
+
+		
+		<div class="col-md-6" id="rightcontent3"> 
+		<h1> About me <h1>
+		<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ac fringilla enim, vitae ultrices justo. Curabitur in dolor interdum, feugiat augue vel, congue purus. Quisque vitae diam turpis. Sed vestibulum porta augue, id interdum tellus interdum consectetur. Etiam sagittis nisi vel mi rutrum elementum. In non placerat nibh, eget dignissim urna. In malesuada risus arcu, sed ornare massa tempus ac. Sed sodales sapien id eros congue, at dictum risus laoreet. Duis sed quam eu nibh lobortis dapibus sed eget quam. Aliquam finibus, magna consectetur convallis gravida, nibh tortor eleifend nisi, in congue lorem lacus vitae sapien. Nunc ac efficitur dui, porta bibendum odio.
+ </p>
+
+		</div>
+		<div class="col-md-6" id="leftcontent3"> 
+
+
+		</div>
+
+	</div>
+
+</div>
+
+<div class="container-fluid" id="maincontent4">
+	<div class="row">
+
+		<div class="col-md-6" id="leftcontent4"> 
+		<h1> My blog <h1>
+		<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ac fringilla enim, vitae ultrices justo. Curabitur in dolor interdum, feugiat augue vel, congue purus. Quisque vitae diam turpis. Sed vestibulum porta augue, id interdum tellus interdum consectetur. Etiam sagittis nisi vel mi rutrum elementum. In non placerat nibh, eget dignissim urna. In malesuada risus arcu, sed ornare massa tempus ac. Sed sodales sapien id eros congue, at dictum risus laoreet. Duis sed quam eu nibh lobortis dapibus sed eget quam. Aliquam finibus, magna consectetur convallis gravida, nibh tortor eleifend nisi, in congue lorem lacus vitae sapien. Nunc ac efficitur dui, porta bibendum odio.
+
+</p>
+
+		</div>
+		<div class="col-md-6" id="rightcontent4"> 
+
+
+		</div>
+
+	</div>
+
+</div>
+</div>
+
+
+
+<?php 
+
+require 'footer.php';
+
+?>
