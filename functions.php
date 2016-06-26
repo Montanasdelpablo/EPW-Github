4bd5e2a98d582644973ea8b824c8e85f59aa6ae8
Pablo Cleij <pablocley@hotmail.com>
2016-06-26 22:28:05 +0200

    Added CPT Profiel & CPT Blog

--- functions.php
+++ functions.php
@@ -27,8 +27,8 @@ add_image_size ('portfolio-item-frontpage', 230, 180);
 
 // custom post types
 
-add_action( 'init', 'create_posttype' );
-function create_posttype() {
+add_action( 'init', 'create_portfolio_posttype');
+function create_portfolio_posttype() {
   register_post_type( 'portfolio',
     array(
       'labels' => array(
@@ -42,5 +42,38 @@ function create_posttype() {
     )
   );
 }
+
+add_action( 'init', 'create_profiel_posttype' );
+function create_profiel_posttype() {
+  register_post_type( 'profiel',
+    array(
+      'labels' => array(
+        'name' => __( 'mijn Profiel'),
+        'singular_name' => __( 'Profiel' )
+      ),
+      'public' => true,
+      'has_archive' => true,
+      'rewrite' => array('slug' => 'mijnprofiel'),
+      'supports' => array('thumbnail', 'title', 'editor', 'author', 'comments'),
+    )
+  );
+}
+
+add_action( 'init', 'create_blog_posttype' );
+function create_blog_posttype() {
+  register_post_type( 'blog',
+    array(
+      'labels' => array(
+        'name' => __( 'mijn Blog'),
+        'singular_name' => __( 'Blog' )
+      ),
+      'public' => true,
+      'has_archive' => true,
+      'rewrite' => array('slug' => 'mijnblog'),
+      'supports' => array('thumbnail', 'title', 'editor', 'author', 'comments'),
+    )
+  );
+}
+
 
 ?>
