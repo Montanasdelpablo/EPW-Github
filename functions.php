569768b4580b80bef129ea047938b7965c7b9dbf
Pablo Cleij <pablocley@hotmail.com>
2016-06-26 16:10:00 +0200

    Included jQuery

--- functions.php
+++ functions.php
@@ -1,5 +1,20 @@
 <?php 
 
+// show admin bar
  show_admin_bar( false ); 
+
+// enqueue scripts
+ function enqueuescripts () {
+ 	wp_enqueue_script("jquery");
+ 	
+ }
+
+ if (function_exists("enqueuescripts")) {
+ 	enqueuescripts();
+ }
+
+
+// include jquery 
+
 
 ?>
