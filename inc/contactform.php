182c89f664eb1af53bcca54c6b0792b9b5ca5ce6
Pablo Cleij <pablocley@hotmail.com>
2016-06-27 01:48:56 +0200

    Added contactform and sidebar to inc folder

---
+++ inc/contactform.php
@@ -0,0 +1,33 @@
+<form class="col-md-6 col-md-offset-3">
+  <fieldset class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter your email">
+    <small class="text-muted">I will never share your email with anyone else.</small>
+  </fieldset>
+ 
+  <fieldset class="form-group">
+    <label for="exampleTextarea">Send a message</label>
+    <textarea class="form-control" id="exampleTextarea" rows="3"></textarea>
+  </fieldset>
+  
+  <div class="radio">
+    <label>
+      <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
+     I want to work on a project with you
+    </label>
+  </div>
+  <div class="radio">
+    <label>
+      <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
+      I would like to hire you
+    </label>
+  </div>
+  <div class="radio disabled">
+    <label>
+      <input type="radio" name="optionsRadios" id="optionsRadios3" value="option3" disabled>
+      I have a question
+    </label>
+  </div>
+  
+  <button type="submit" class="btn btn-danger">Submit</button>
+</form>
