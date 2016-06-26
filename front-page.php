a96e69ce97ae85f24482a9dc7e19eb82e4a2995f
Pablo Cleij <pablocley@hotmail.com>
2016-06-26 18:48:40 +0200

    Added CPT: Portfolio items loop

--- front-page.php
+++ front-page.php
@@ -46,7 +46,33 @@ require 'header.php';
 		</div>
 		
 		<div class="col-md-6" id="rightcontent2"> 
+		<div class="row">
+			<?php 
 
+			$args = array( 'post_type' => 'portfolio', 'posts_per_page' => 10 );
+			$loop = new WP_Query( $args );
+			
+			while ( $loop->have_posts() ) : $loop->the_post();
+			echo "<div id='portfolioitem' class='col-md-3'>";
+
+			echo "<div class='entry-content'>";
+	  	    echo "<h3>";
+	  	    	the_title();
+	  	   	echo "</h3>";
+				
+			  echo '</div>';
+				echo "<div class='row'>";
+					the_post_thumbnail();
+		  	    echo "</div>";
+
+		  	    echo "<div class='row'>";
+					echo "<button> <a> Lees meer </a> </button>";
+		  	    echo "</div>";
+			echo "</div>";
+			endwhile;
+
+			?>
+		</div>
 
 		</div>
 
@@ -57,13 +83,13 @@ require 'header.php';
 	<div class="row">
 
 		
-		<div class="col-md-6" id="rightcontent3"> 
+		<div class="col-md-6" id="leftcontent3"> 
 		<h1> About me <h1>
 		<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ac fringilla enim, vitae ultrices justo. Curabitur in dolor interdum, feugiat augue vel, congue purus. Quisque vitae diam turpis. Sed vestibulum porta augue, id interdum tellus interdum consectetur. Etiam sagittis nisi vel mi rutrum elementum. In non placerat nibh, eget dignissim urna. In malesuada risus arcu, sed ornare massa tempus ac. Sed sodales sapien id eros congue, at dictum risus laoreet. Duis sed quam eu nibh lobortis dapibus sed eget quam. Aliquam finibus, magna consectetur convallis gravida, nibh tortor eleifend nisi, in congue lorem lacus vitae sapien. Nunc ac efficitur dui, porta bibendum odio.
  </p>
 
 		</div>
-		<div class="col-md-6" id="leftcontent3"> 
+		<div class="col-md-6" id="rightcontent3"> 
 
 
 		</div>
