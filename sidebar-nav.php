85a7dff5574574433654255fd095a4098fe21e6c
Pablo Cleij <pablocley@hotmail.com>
2016-06-26 16:10:59 +0200

    Added navigation bar for frontpage

---
+++ sidebar-nav.php
@@ -0,0 +1,41 @@
+<div id="wrapper">
+        <div class="overlay">
+        	
+        </div>
+    
+        <!-- Sidebar -->
+        <nav class="navbar navbar-inverse navbar-fixed-top" id="sidebar-wrapper" role="navigation">
+            <ul class="nav sidebar-nav">
+                <li class="sidebar-brand">
+                    <a href="#">
+                       Pablo
+                    </a>
+                </li>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">About me</a>
+                </li>
+               
+              
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Portfolio <span class="caret"></span></a>
+                  <ul class="dropdown-menu" role="menu">
+                    
+                    <li><a href="#">School</a></li>
+                    <li><a href="#">Work</a></li>
+                    <li><a href="#">Personal</a></li>
+                   
+                  </ul>
+                </li>
+
+                 <li>
+                    <a href="#">My blog </a>
+                </li>
+              
+            </ul>
+        </nav>
+        
+      
+      
