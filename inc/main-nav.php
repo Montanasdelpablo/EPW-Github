8160f0891975bf0bc7f931151da1c330a86ce0d8
Pablo Cleij <pablocley@hotmail.com>
2016-06-27 18:34:40 +0200

    Changed main nav

--- inc/main-nav.php
+++ inc/main-nav.php
@@ -6,26 +6,27 @@
 						<div class="toggle" style='display:none'>
 							<a class="toggleMenu" href="#"><?php _e('Menu','epw'); ?></a>
 						</div> 	
+						<div class="row">
+							<div class="col-md-12 main-nav">
 
-						<div class="main-nav">
-
-							
-							<?php wp_nav_menu( array(
-								'theme_location'  => 'primary', 
-								'menu'            => 'Primary Menu',
-								'container'       => 'div',
-								'container_class' => 'menu-{menu slug}-container',
-								'container_id'    => 'cssmenu',
-								'menu_class'      => 'menu',
-								'menu_id'         => '',
-								'echo'            => true,
-								'before'          => '',
-								'after'           => '',
-								'link_before'     => '',
-								'link_after'      => '',
-								'items_wrap'      => '<ul>%3$s</ul>'
-								) ); ?>							
-						</div>						
+								
+								<?php wp_nav_menu( array(
+									'theme_location'  => 'primary', 
+									'menu'            => 'Primary Menu',
+									'container'       => 'div',
+									'container_class' => 'col-md-6 col-md-offset-3',
+									'container_id'    => 'cssmenu',
+									'menu_class'      => 'menu',
+									'menu_id'         => '',
+									'echo'            => true,
+									'before'          => '',
+									'after'           => '',
+									'link_before'     => '',
+									'link_after'      => '',
+									'items_wrap'      => '<ul class="row">%3$s</ul>'
+									) ); ?>							
+							</div>	
+						</div>					
 				<div class="clear"></div>
 
  </div>
