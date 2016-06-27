7e48b4291cc5a264f5105657224d13580d6c6c6b
Pablo Cleij <pablocley@hotmail.com>
2016-06-27 18:33:48 +0200

    Added 404 page for error handling

---
+++ 404.php
@@ -0,0 +1,32 @@
+<?php 
+require 'header.php';
+
+?>
+
+
+<div id="portfolio" class="container">
+	
+
+	<div id="error" class="row">
+
+		<div class="col-md-6 col-md-offset-3">
+
+		<span id="ohno"> Oh no! :( </span>
+
+		</div>
+		
+
+		<div class="col-md-6 col-md-offset-3">
+
+		<h1> I'm sorry! Something has gone wrong. Try refreshing the website! </h1>
+			
+		</div>
+	</div>
+
+
+
+
+<?php 
+require 'footer.php';
+?>
+</div>
